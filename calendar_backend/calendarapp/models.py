1524221df3c4e9296e784eeb0d4c65b8cfcdf676
Ifeadewumi <61970764+Ifeadewumi@users.noreply.github.com>
2021-08-31 19:09:43 +0100

    Fixed issue #151 created working event model and migrated

--- calendar_backend/calendarapp/models.py
+++ calendar_backend/calendarapp/models.py
@@ -1,9 +1,22 @@
+from time import tzname
 from django.db import models
+from django.utils.timezone import now
 
 
 
 class Event(models.Model):
-    pass
+    title = models.CharField(max_length=500)
+    start_date = models.DateField(now)
+    end_date = models.DateField()
+    start_time = models.DateTimeField(now)
+    end_time = models.DateTimeField()
+    description = models.CharField(max_length=100000)
+    event_tag = models.CharField(max_length=100)
+    event_color = models.CharField(max_length=100)
+    availability = models.BooleanField()
+    visibility = models.CharField(max_length=100)
+    
+
 
 
 class Reminder(models.Model):
