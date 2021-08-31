9f8047b5c2b3e9fdc2bf7033ff894b9217f28f58
jamido1 <66392050+jamido1@users.noreply.github.com>
2021-08-31 22:53:25 +0100

    added a midddleware.py file and linked it to the settings.py, I also added a core app containing utilities for get_timezone and default_timezone

---
+++ calendar_backend/calendar_backend/middleware.py
@@ -0,0 +1,23 @@
+import pytz
+
+from django.utils import timezone
+
+
+class TimezoneMiddleware(object):
+    """
+    Middleware class that allows us to set the current timezone
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        return self.get_response(request)
+
+    def process_request(self, request):
+
+        tzname = request.session.get('django_timezone')
+        if tzname:
+            timezone.activate(pytz.timezone(tzname))
+        else:
+            timezone.deactivate()
