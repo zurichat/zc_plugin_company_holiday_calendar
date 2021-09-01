d82f6c8a8cea9be6ff13c31ea2ab05fefbca1ee5
Ifeadewumi <61970764+Ifeadewumi@users.noreply.github.com>
2021-09-01 08:45:30 +0100

    Changed event title to event_name

--- calendar_backend/calendarapp/models.py
+++ calendar_backend/calendarapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Event(models.Model):
-    title = models.CharField(max_length=500)
+    event_name = models.CharField(max_length=500)
     start_date = models.DateField()
     end_date = models.DateField()
     start_time = models.TimeField()
@@ -17,7 +17,7 @@ class Event(models.Model):
     visibility = models.CharField(max_length=100)
     
     def __str__(self):
-        return self.title
+        return self.event_name
 
 
 
