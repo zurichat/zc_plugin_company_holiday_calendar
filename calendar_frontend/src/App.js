dd2981c289d1318c1e082aab70d59dbf84fd698f
Adeoye Victor <adeoyevictor16@gmail.com>
2021-09-01 12:53:22 +0100

    added two buttons to open and close the event form sidebar

--- calendar_frontend/src/App.js
+++ calendar_frontend/src/App.js
@@ -1,58 +1,66 @@
-import format from "date-fns/format";
-import getDay from "date-fns/getDay";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import React from "react";
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
-import "./css/react-big-calendar.css";
-
+import format from 'date-fns/format'
+import getDay from 'date-fns/getDay'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import React from 'react'
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import './css/react-big-calendar.css'
+import Event from './conponents/Event/Event'
 
 const locales = {
-    "en-US": require("date-fns/locale/en-US"),
-};
+  'en-US': require('date-fns/locale/en-US'),
+}
 const localizer = dateFnsLocalizer({
-    format,
-    parse,
-    startOfWeek,
-    getDay,
-    locales,
-});
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
 
 const events = [
-    {
-        title: "Big Meeting",
-        allDay: true,
-        start: new Date(2021, 6, 0),
-        end: new Date(2021, 6, 0),
-    },
-    {
-        title: "Vacation",
-        start: new Date(2021, 6, 7),
-        end: new Date(2021, 6, 10),
-    },
-    {
-        title: "Conference",
-        start: new Date(2021, 6, 20),
-        end: new Date(2021, 6, 23),
-    },
-];
+  {
+    title: 'Big Meeting',
+    allDay: true,
+    start: new Date(2021, 6, 0),
+    end: new Date(2021, 6, 0),
+  },
+  {
+    title: 'Vacation',
+    start: new Date(2021, 6, 7),
+    end: new Date(2021, 6, 10),
+  },
+  {
+    title: 'Conference',
+    start: new Date(2021, 6, 20),
+    end: new Date(2021, 6, 23),
+  },
+]
 
 function App() {
-
-    return (
-        <div className="App">
-          <div style={{padding: "30px 47px 0px"}}>
-            <h1>Calendar</h1>
-            <h2>Add New Event</h2>
-            </div>
-            <Calendar className="calendar" localizer={localizer} events={events} startAccessor="start" endAccessor="end" style={{ height: 500, margin: "50px" }} />
+  return (
+    <>
+      <Event />
+      <div className='App'>
+        <div style={{ padding: '30px 47px 0px' }}>
+          <h1>Calendar</h1>
+          <h2>Add New Event</h2>
         </div>
-    );
+        <Calendar
+          className='calendar'
+          localizer={localizer}
+          events={events}
+          startAccessor='start'
+          endAccessor='end'
+          style={{ height: 500, margin: '50px' }}
+        />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
 
 // packages to install before use
 
 // npm i --save react-big-calendar react-datepicker date-fns
-
