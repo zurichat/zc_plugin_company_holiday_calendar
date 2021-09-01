739c9f2b47f2618ba2d0c0a32f7d2884fc93e126
akanjimm <akanjimm@gmail.com>
2021-09-01 22:57:45 +0100

    calendar event update view and endpoint were created -- issue #158

--- calendar_backend/calendarapp/views.py
+++ calendar_backend/calendarapp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
+# update event view imports
+from rest_framework import generics
+from .models import Event
+from .serializers import EventSerializer
+
 # Create your views here.
 
 
 def calendar_view(request):
     return HttpResponse("This is where all calender activities are performed and displayed")
+
+
+class EventUpdateView(generics.UpdateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
