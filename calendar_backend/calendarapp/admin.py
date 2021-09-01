8f19e4b08c5257f244a2a64361f3307789052751
tayrexfmt <tayrexfmt@gmail.com>
2021-09-01 21:42:09 +0100

    second commit created working reminder model and migrated

--- calendar_backend/calendarapp/admin.py
+++ calendar_backend/calendarapp/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 
 # Register your models here.
-from .models import Event
+from .models import Event, Reminder
 admin.site.register(Event)
+admin.site.register(Reminder)
 
--- calendar_backend/calendarapp/models.py
+++ calendar_backend/calendarapp/models.py
@@ -1,6 +1,7 @@
 #from os import O_NDELAY
 from types import DynamicClassAttribute
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 from django.db.models.fields import AutoField
 # from django.utils.timezone import now
@@ -26,18 +27,17 @@ class Event(models.Model):
 
 
 class Reminder(models.Model):
-    reminder_id = models.CharField(max_length=100000)
-    event_name = models.CharField(max_length=500)
-    date = models.DateField()
-    time = models.TimeField()
-    event_description = models.CharField(max_length=100000)
-    custom_occurence = models.CharField(max_length=1000)
-    repeat_every = models.IntegerField()
-    repeat_on = models.CharField(max_length=10)
-    ends = models.CharField(max_length=50)
-    never = models.CharField(max_length=500)
-    on_occurence = models.DateField()
-    after_occurence = models.CharField(max_length=500)
+    event_name = models.CharField(max_length=500, null=True)
+    date = models.DateField(null=True)
+    time = models.TimeField(null=True)
+    event_description = models.CharField(max_length=100000, null=True)
+    custom_occurence = models.CharField(max_length=1000, null=True)
+    repeat_every = models.IntegerField(null=True)
+    repeat_on = models.CharField(max_length=10, null=True)
+    ends = models.CharField(max_length=50, null=True)
+    never = models.CharField(max_length=500, null=True)
+    on_occurence = models.DateField(null=True)
+    after_occurence = models.CharField(max_length=500, null=True)
 
     def __str__(self):
         return self.event_name
