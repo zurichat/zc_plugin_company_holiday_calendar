85cf18451a110c9119037fcec59750e1af2ecb29
Ifeadewumi <61970764+Ifeadewumi@users.noreply.github.com>
2021-09-01 07:56:01 +0100

    Updated timezone field to take character entries from tz database list. See comment in Event model for more details

--- calendar_backend/calendarapp/models.py
+++ calendar_backend/calendarapp/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 
 
 
 class Event(models.Model):
     title = models.CharField(max_length=500)
-    start_date = models.DateField(now)
+    start_date = models.DateField()
     end_date = models.DateField()
     start_time = models.TimeField(auto_now_add=True)
     end_time = models.TimeField()
-    time_zone = models.TimeField(now) 
+    time_zone = models.CharField(max_length=250)  # able to accept any entry from the database names in the List of tz database time zones (https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
     description = models.CharField(max_length=100000)
     event_tag = models.CharField(max_length=100)
     event_color = models.CharField(max_length=100)
