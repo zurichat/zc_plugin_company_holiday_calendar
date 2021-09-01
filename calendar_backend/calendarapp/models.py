a089f6acc2e1d262f78f9c65e5d6c9eef36a2286
Ifeadewumi <61970764+Ifeadewumi@users.noreply.github.com>
2021-09-01 08:29:34 +0100

    Updated start_time field

--- calendar_backend/calendarapp/models.py
+++ calendar_backend/calendarapp/models.py
@@ -7,7 +7,7 @@ class Event(models.Model):
     title = models.CharField(max_length=500)
     start_date = models.DateField()
     end_date = models.DateField()
-    start_time = models.TimeField(auto_now_add=True)
+    start_time = models.TimeField()
     end_time = models.TimeField()
     time_zone = models.CharField(max_length=250)  # able to accept any entry from the database names in the List of tz database time zones (https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
     description = models.CharField(max_length=100000)
