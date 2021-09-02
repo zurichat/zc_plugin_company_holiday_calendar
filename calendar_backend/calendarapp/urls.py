16b3c84377cb71cf4ea61766667b1715a9db3705
Ramish Benoit <lottinbudzi12@gmail.com>
2021-09-02 22:47:26 +0100

    Added the reminder list view and URL

--- calendar_backend/calendarapp/urls.py
+++ calendar_backend/calendarapp/urls.py
@@ -6,6 +6,7 @@ from .views import *
 
 urlpatterns = [
     path('', calendar_view, name="calendar_view"),
-    path('update-event/<int:pk>', EventUpdateView.as_view()),
-    path('search/', EventSearch.as_view(), name="EventSearch"),
+    # path('update-event/<int:pk>', EventUpdateView.as_view()),
+    # path('search/', EventSearch.as_view(), name="EventSearch"),
+    path('reminders/', ReminderListView.as_view(), name='reminders')
 ]
--- calendar_backend/calendarapp/views.py
+++ calendar_backend/calendarapp/views.py
@@ -8,7 +8,8 @@ from . models import *
 # update event view imports
 from rest_framework import generics
 from .models import Event
-from .serializers import EventSerializer
+from .serializers import ReminderSerializer
+# from .serializers import EventSerializer
 
 # Create your views here.
 
@@ -18,14 +19,24 @@ def calendar_view(request):
     return HttpResponse("This is where all calender activities are performed and displayed")
 
 
-class EventUpdateView(generics.UpdateAPIView):
-    queryset = Event.objects.all()
-    serializer_class = EventSerializer
+# class EventUpdateView(generics.UpdateAPIView):
+#     queryset = Event.objects.all()
+#     serializer_class = EventSerializer
 
 
-class EventSearch(generics.ListAPIView):
-    search_fields = ['event_name','start_date']
-    filter_backends = (filters.SearchFilter,)
-    queryset = Event.objects.all()
-    serializer_class = EventSerializer #Eventserializer needed!
+# class EventSearch(generics.ListAPIView):
+#     search_fields = ['event_name','start_date']
+#     filter_backends = (filters.SearchFilter,)
+#     queryset = Event.objects.all()
+#     serializer_class = EventSerializer #Eventserializer needed!
+
+class ReminderListView(generics.ListCreateAPIView):
+    queryset = Reminder.objects.all()
+    serializer_class = ReminderSerializer
+
+    # def list(self, request):
+    #     # Note the use of `get_queryset()` instead of `self.queryset`
+    #     queryset = self.get_queryset()
+    #     serializer = ReminderSerializer(queryset, many=True)
+    #     return Response(serializer.data)
 
