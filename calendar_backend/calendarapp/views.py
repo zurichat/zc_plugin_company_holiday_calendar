7105e0c5612169efd5054eab63b29e2fd0c08084
Ramish Benoit <lottinbudzi12@gmail.com>
2021-09-02 22:51:54 +0100

    Updated the view to take the Reminder model

--- calendar_backend/calendarapp/views.py
+++ calendar_backend/calendarapp/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from rest_framework import generics
 from rest_framework import filters
-# from .serializers import EventSerializer
+#from .serializers import EventSerializer
 from . models import *
 
 # update event view imports
 from rest_framework import generics
-from .models import Event
-from .serializers import ReminderSerializer
-# from .serializers import EventSerializer
+from .models import Event, Reminder
+from .serializers import EventSerializer, ReminderSerializer
 
 # Create your views here.
 
@@ -19,16 +18,16 @@ def calendar_view(request):
     return HttpResponse("This is where all calender activities are performed and displayed")
 
 
-# class EventUpdateView(generics.UpdateAPIView):
-#     queryset = Event.objects.all()
-#     serializer_class = EventSerializer
+class EventUpdateView(generics.UpdateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
 
 
-# class EventSearch(generics.ListAPIView):
-#     search_fields = ['event_name','start_date']
-#     filter_backends = (filters.SearchFilter,)
-#     queryset = Event.objects.all()
-#     serializer_class = EventSerializer #Eventserializer needed!
+class EventSearch(generics.ListAPIView):
+    search_fields = ['event_name','start_date']
+    filter_backends = (filters.SearchFilter,)
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer #Eventserializer needed!
 
 class ReminderListView(generics.ListCreateAPIView):
     queryset = Reminder.objects.all()
