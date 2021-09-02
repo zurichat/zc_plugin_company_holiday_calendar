ab91788ca7b9de3347da493c5a8801494cefb69b
Olalekan Bello <olalekanbello534@gmail.com>
2021-09-02 11:52:32 +0100

    Create and Cancel buttons added as per ticket #124

--- calendar_frontend/src/conponents/Event.js
+++ calendar_frontend/src/conponents/Event.js
@@ -1,34 +1,40 @@
-import React, { useState } from 'react';
-import './event.css';
-import { FaTimesCircle } from 'react-icons/fa';
-import DateInput from './DateInput';
-import ShowMe from './showMe/ShowMe';
+import React, { useState } from "react";
+import "./event.css";
+import { FaTimesCircle } from "react-icons/fa";
+import DateInput from "./DateInput";
+import ShowMe from "./showMe/ShowMe";
 
 const Event = () => {
   const [isEventOpen, setIsEventOpen] = useState(false);
   return (
     <div>
-      <button id='add-event' onClick={() => setIsEventOpen(true)}>
+      <button id="add-event" onClick={() => setIsEventOpen(true)}>
         Add Event
       </button>
-      {isEventOpen && <div className='overlay'></div>}
+      {isEventOpen && <div className="overlay"></div>}
       {isEventOpen && (
-        <div className='event-form'>
+        <div className="event-form">
           <header>
             <h2>Add New Event</h2>
             <FaTimesCircle
-              className='faTimes'
+              className="faTimes"
               onClick={() => setIsEventOpen(false)}
             />
           </header>
           <h3>Add New Event</h3>
           <div>
-            <input type='text' placeholder='Add Title' />
-            <DateInput placeholder='Start Date' />
-            <DateInput placeholder='End Date' />
-            <button stlye={{ marginTop: '10px' }}>Add Event</button>
+            <input type="text" placeholder="Add Title" />
+            <DateInput placeholder="Start Date" />
+            <DateInput placeholder="End Date" />
+            <button stlye={{ marginTop: "10px" }}>Add Event</button>
           </div>
           <ShowMe />
+          {/* Code for Create and Cancel buttons on Event form */}
+          <section className="cancel-create">
+            <button id="cancelButton">Cancel</button>
+            <button id="createButton">Create</button>
+          </section>
+          {/* End of Code for Create and Cancel button */}
         </div>
       )}
     </div>
