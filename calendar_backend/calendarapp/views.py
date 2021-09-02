9bc73f75a426700d92d344ac81a222e92ec1c5f0
50-Course <erioluwaadeodu@my.uopeople.edu>
2021-09-03 06:23:59 +0800

    Tickeet #153 update

--- calendar_backend/calendarapp/views.py
+++ calendar_backend/calendarapp/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import HttpResponse
-
+from rest_framework import serializers
+from rest_framework.viewsets import ModelViewSet
+from calendarapp.serializers import EventSerializer
+from calendarapp.models import Event
 
 
 def calendar_view(request):
     return HttpResponse("This is where all calender activities are performed and displayed")
 
 
-
-
-
-class EventViewsets(viewsets.ModelViewSet):
+class EventViewsets(ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
