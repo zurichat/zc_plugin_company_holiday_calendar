8b09af2cd301c5ac3c4e98c1d0eb1649ed56a2e7
jephthah <jsnr300@gmail.com>
2021-09-02 12:48:10 +0100

    Added search functionality to the plugin 'search/'

--- calendar_backend/calendarapp/urls.py
+++ calendar_backend/calendarapp/urls.py
@@ -5,5 +5,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('', calendar_view, name="calendar_view")
+    path('', calendar_view, name="calendar_view"),
+    path('search/', EventSearch.as_view(), name="EventSearch")
 ]
