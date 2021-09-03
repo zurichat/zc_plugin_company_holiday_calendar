4028c84f930877d12ea723349870e21d904f92aa
Adeoye Victor <adeoyevictor16@gmail.com>
2021-09-03 22:31:38 +0100

    added responsiveness

--- calendar_frontend/src/components/Card.js
+++ calendar_frontend/src/components/Card.js
@@ -1,55 +1,52 @@
-import React from 'react';
+import React from 'react'
 import './Card.css'
-const randomData=[
-    {
-        day:"Wednesday",
-        date:'01',
-        timings:'All Day',
-        description:"Company Break"
-    },
-    {
-        day:"Thursday",
-        date:'02',
-        timings:'All Day',
-        description:"HNGi8 Hangout"
-    },
-    {   
-        day:"Friday",
-        date:'03',
-        timings:'All Day',
-        description:"Company Break"
-    },
-    {
-        day:"Saturday",
-        date:'04',
-        timings:'12:00pm - 8:00',
-        description:"Zuri Picnic"
-    }
+const randomData = [
+  {
+    day: 'Wednesday',
+    date: '01',
+    timings: 'All Day',
+    description: 'Company Break',
+  },
+  {
+    day: 'Thursday',
+    date: '02',
+    timings: 'All Day',
+    description: 'HNGi8 Hangout',
+  },
+  {
+    day: 'Friday',
+    date: '03',
+    timings: 'All Day',
+    description: 'Company Break',
+  },
+  {
+    day: 'Saturday',
+    date: '04',
+    timings: '12:00pm - 8:00',
+    description: 'Zuri Picnic',
+  },
 ]
-const Card=()=>{
-
-    const Eventfunction=()=>{
-        return randomData.map((data)=>{
-            return <div className="card" >
-                <div className="card-body">
-                    <div className="d-flex flex-column">
-                        <div className="d-flex fw-bolder">
-                            <div className="card-text">{data.day}</div>
-                            <div className="card-text ms-3">{data.date}</div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fas fa-sun"></i>
-                        </div>
-                    <div className="timings">{data.timings}</div>
-                <div className="description">{data.description}</div>
+const Card = () => {
+  const Eventfunction = () => {
+    return randomData.map((data, index) => {
+      return (
+        <div className='card' key={index}>
+          <div className='card-body'>
+            <div className='d-flex flex-column'>
+              <div className='d-flex fw-bolder'>
+                <div className='card-text'>{data.day}</div>
+                <div className='card-text ms-3'>{data.date}</div>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class='fas fa-sun'></i>
+              </div>
+              <div className='timings'>{data.timings}</div>
+              <div className='description'>{data.description}</div>
             </div>
-           
+          </div>
         </div>
-        </div>
+      )
     })
-    }
-    return(
-        <div className="container">
-           {Eventfunction()}
-        </div>
-    )
+  }
+  return <div className='container'>{Eventfunction()}</div>
 }
 
 export default Card
