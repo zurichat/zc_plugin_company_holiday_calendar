b87b6132da99a3114051caf6dcae8e6dbb88696c
utibeabasi6 <utibeabasiumanah6@gmail.com>
2021-09-03 23:00:13 +0100

    initial config for frontend

--- calendar_backend/calendar_backend/views.py
+++ calendar_backend/calendar_backend/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 
 
 
 def homepage(request):
-    return HttpResponse('<h1>Welcome to Zuri Chat Company Holiday Calendar: Edited with ci/cd</h1>')
+    return render(request, "index.html")
