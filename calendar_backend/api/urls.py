a2851e75223c06e9c0a2999a9aaa8296be1d25b4
Olowu Abayomi <aayobam@gmail.com>
2021-09-03 12:00:56 +0100

    Revert "Ticket #153: Serializer class for Event"

--- calendar_backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-from .views import EventViewsets
-
-router = routers.DefaultRouter()
-
-router.register(r'v1', EventViewsets, basename='events')
-
-
-urlpatterns = router.urls
--- calendar_backend/calendarapp/urls.py
+++ calendar_backend/calendarapp/urls.py
@@ -1,18 +1,12 @@
+from django.contrib.staticfiles.urls import urlpatterns
 from django.urls import path
-from rest_framework import routers
-from .views import calendar_view, EventUpdateView, EventSearch, EventViewsets
+from .views import calendar_view, EventUpdateView, EventSearch
 
 
-
-router = routers.DefaultRouter()
-
-router.register(r'v2', EventViewsets, basename='events')
 
 urlpatterns = [
     path('', calendar_view, name="calendar_view"),
     path('update-event/<int:pk>', EventUpdateView.as_view()),
     path('search/', EventSearch.as_view(), name="EventSearch"),
-    path('reminders/', ReminderListView.as_view(), name='reminders'),
-] 
-
-urlpatterns += router.urls
+    path('reminders/', ReminderListView.as_view(), name='reminders')
+]
