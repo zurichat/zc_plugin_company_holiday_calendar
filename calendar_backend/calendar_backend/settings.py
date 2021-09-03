05cdb98b208482bf3496a4a7fd8c35cc5641e117
olowu abayomi <aayobam@gmail.com>
2021-09-03 22:57:35 +0100

    update

--- calendar_backend/calendar_backend/settings.py
+++ calendar_backend/calendar_backend/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'calendar_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- calendar_backend/calendar_backend/views.py
+++ calendar_backend/calendar_backend/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
 
 
 
