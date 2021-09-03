77b570e11cf8b641628371886d3f03fc07afd6e9
Elizabeth Eli <elizabetheli4u@gmail.com>
2021-09-03 12:28:43 +0100

    Created an endpoint that displays information about the plugin in Json format

--- calendar_backend/calendar_backend/urls.py
+++ calendar_backend/calendar_backend/urls.py
@@ -4,6 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from .views import homepage
+from calendarapp.views import *
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +15,10 @@ urlpatterns = [
     path('', homepage, name="home_page"),
     path('api/', include('api.urls')),
     path('zuricalendar/', include('calendarapp.urls')),
+    path('sidebar', side_bar_view, name="side_bar"),
+    path('info', plugin_info_view, name="plugin_info"),
+    path('ping', ping_view, name="ping")
+
 ]
 
 
--- calendar_backend/calendarapp/views.py
+++ calendar_backend/calendarapp/views.py
@@ -1,24 +1,68 @@
+from django.shortcuts import render
 from django.shortcuts import HttpResponse
-from rest_framework.viewsets import ModelViewSet
-
+from django.http import JsonResponse
 from rest_framework import generics
 from rest_framework import filters
-
+from .serializers import EventSerializer
 from . models import *
 from rest_framework import generics
+from .serializers import EventSerializer
 from .models import Event, Reminder
 from .serializers import EventSerializer, ReminderSerializer
 
 
 
+
+# Create your views here.
+
+# Create your views here.
+
 def calendar_view(request):
     return HttpResponse("This is where all calender activities are performed and displayed")
 
+# creating a  view for  displaying  the plugin information as a static Json object.
+
+def plugin_info_view(request):
+    plugin_information = [
+        {'status': 'Success',
+        'name' : 'Company_Holiday_Calendar Plugin',
+        'type':'Plugin Information',  
+        'description' : ['Company Holiday Calender Plugin is a zuri.chat plugin  that enables  calender functions mainly to organize or schedule holidays or events for a company or a team']},
+        
+        {'scaffold_structure' :'Monolith',
+        'team' : 'HNG-8.0/Team-Hemingway',
+        'sidebar_url' : 'http://calendar.zuri.chat/sidebar',
+        'homepage_url': "http://calendar.zuri.chat/",
+        'ping_url': 'http://calendar.zuri.chat/ping',
+        }
+    ]
+        
+    return JsonResponse({'plugin_information': plugin_information})
 
 
-class EventViewsets(ModelViewSet):
-    queryset = Event.objects.all()
-    serializer_class = EventSerializer
+# creating a  view for  displaying  the plugin side bar as a static Json object.
+
+def side_bar_view(request):
+    side_bars = [
+        {'plugin':'plugin for Zuri Chat that enables  calender functions',
+        'status': 'Success',
+        'type':'Plugin sidebar'}
+    ]
+        
+    return JsonResponse({'side_bars': side_bars})
+
+# creating a  view for  displaying  a report on the functionality of the server as a static Json object.
+
+def ping_view(request):
+    server = [
+        {'status': 'Success',
+        'Report': ['This server is working']}
+    ]
+    return JsonResponse({'server': server})
+
+
+
+
 
 class EventUpdateView(generics.UpdateAPIView):
     queryset = Event.objects.all()
@@ -29,7 +73,6 @@ class EventSearch(generics.ListAPIView):
     search_fields = ['event_name','start_date']
     filter_backends = (filters.SearchFilter,)
     queryset = Event.objects.all()
-
     serializer_class = EventSerializer #Eventserializer needed!
 
     serializer_class = EventSerializer #Eventserializer needed!
