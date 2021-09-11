c71b33e6c5c017b79d5e6889c7ef0032e81865fd
Adeoye Victor <adeoyevictor16@gmail.com>
2021-09-11 13:56:39 +0100

    codebase restructure

---
+++ calendar_frontend/src/components/HolidayList/HolidayList.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './HolidayList.css'
+
+const HolidayList = () => {
+  return <div className='holiday-list'></div>
+}
+
+export default HolidayList
---
+++ calendar_frontend/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Sidebar.css'
+
+const Sidebar = () => {
+  return <div className='sidebar'></div>
+}
+
+export default Sidebar
