8a1dab97ac3cb7a193026292fd084cb1b8e5ca12
Elizabeth Eli <elizabetheli4u@gmail.com>
2021-09-11 22:11:22 +0100

    ..

---
+++ calendar_frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return <nav className='nav'></nav>
+}
+
+export default Navbar
---
+++ calendar_frontend/src/components/OverLay/Overlay.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Overlay.css'
+
+const Overlay = () => {
+  return <div></div>
+}
+
+export default Overlay
