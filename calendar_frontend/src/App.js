ac79d14ff1a4a2ba06618c1603f0a1444cb26a09
Adeoye Victor <adeoyevictor16@gmail.com>
2021-09-12 09:46:20 +0100

    added responsiveness

--- calendar_frontend/src/App.js
+++ calendar_frontend/src/App.js
@@ -10,11 +10,7 @@ export const AppContext = React.createContext()
 const App = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
   const [showEventPage, setShowEventPage] = useState(true)
-  const [eventTitle, setEventTitle] = useState('Weekend Get Away')
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(eventTitle)
-  }
+
   return (
     <div className='App'>
       <AppContext.Provider
