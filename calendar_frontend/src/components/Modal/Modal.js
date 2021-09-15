0af6069cea711137e46a4ae835307113b7e8ce8b
SeunPaul <seunpaul148@gmail.com>
2021-09-15 13:08:09 +0100

    added form validation

--- calendar_frontend/src/components/Modal/Modal.js
+++ calendar_frontend/src/components/Modal/Modal.js
@@ -1,5 +1,4 @@
 import React, { useContext, useState } from "react";
-import { format } from 'date-fns'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { FaRegCalendar, FaAngleDown, FaRegEdit, FaRegClock } from 'react-icons/fa';
@@ -7,9 +6,9 @@ import { useForm, Controller } from "react-hook-form";
 import { AppContext } from '../../App'
 import './Modal.css'
 import { HiOutlinePencilAlt, HiOutlineClockCircle } from 'react-icons/hi'
-import icon from './clock-icon.png'
-import icon2 from './calendar-icon.png'
-// import { format } from "date-fns";
+import icon from './Icons/clock-icon.png'
+import icon2 from './Icons/calendar-icon.png'
+import icon3 from './Icons/active.png'
 import { CirclePicker } from "react-color";
 import chevronDown from "./Shape.png";
 import Button from "@material-ui/core/Button";
@@ -25,105 +24,111 @@ function Alert(props) {
 const Modal = () => {
   const states = useContext(AppContext);
   const { isModalOpen, setIsModalOpen, showEventPage, setShowEventPage } = states
-  const { control } = useForm();
+
+  const [color, setColor] = useState("#00B87C");
+  const [isColorBoxOpen, setIsColorBoxOpen] = useState(false);
+  const [eventTag, setEventTag] = useState("");
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+
+  const { register, control, handleSubmit, formState: {errors}, clearErrors } = useForm();
 
   const timeStrings = [
-    {value: "00:00", label: "12:00 AM"},
-    {value: "00:15", label: "12:15 AM"},
-    {value: "00:30", label: "12:30 AM"},
-    {value: "00:45", label: "12:45 AM"},
-    {value: "01:00", label: "01:00 AM"},
-    {value: "01:15", label: "01:15 AM"},
-    {value: "01:30", label: "01:30 AM"},
-    {value: "01:45", label: "01:45 AM"},
-    {value: "02:00", label: "02:00 AM"},
-    {value: "02:15", label: "02:15 AM"},
-    {value: "02:30", label: "02:30 AM"},
-    {value: "02:45", label: "02:45 AM"},
-    {value: "03:00", label: "03:00 AM"},
-    {value: "03:15", label: "03:15 AM"},
-    {value: "03:30", label: "03:30 AM"},
-    {value: "03:45", label: "03:45 AM"},
-    {value: "04:00", label: "04:00 AM"},
-    {value: "04:15", label: "04:15 AM"},
-    {value: "04:30", label: "04:30 AM"},
-    {value: "04:45", label: "04:45 AM"},
-    {value: "05:00", label: "05:00 AM"},
-    {value: "05:15", label: "05:15 AM"},
-    {value: "05:30", label: "05:30 AM"},
-    {value: "05:45", label: "05:45 AM"},
-    {value: "06:00", label: "06:00 AM"},
-    {value: "06:15", label: "06:15 AM"},
-    {value: "06:30", label: "06:30 AM"},
-    {value: "06:45", label: "06:45 AM"},
-    {value: "07:00", label: "07:00 AM"},
-    {value: "07:15", label: "07:15 AM"},
-    {value: "07:30", label: "07:30 AM"},
-    {value: "07:45", label: "07:45 AM"},
-    {value: "08:00", label: "08:00 AM"},
-    {value: "08:15", label: "08:15 AM"},
-    {value: "08:30", label: "08:30 AM"},
-    {value: "08:45", label: "08:45 AM"},
-    {value: "09:00", label: "09:00 AM"},
-    {value: "09:15", label: "09:15 AM"},
-    {value: "09:30", label: "09:30 AM"},
-    {value: "09:45", label: "09:45 AM"},
-    {value: "10:00", label: "10:00 AM"},
-    {value: "10:15", label: "10:15 AM"},
-    {value: "10:30", label: "10:30 AM"},
-    {value: "10:45", label: "10:45 AM"},
-    {value: "11:00", label: "11:00 AM"},
-    {value: "11:15", label: "11:15 AM"},
-    {value: "11:30", label: "11:30 AM"},
-    {value: "11:45", label: "11:45 AM"},
-    {value: "12:00", label: "12:00 PM"},
-    {value: "12:15", label: "12:15 PM"},
-    {value: "12:30", label: "12:30 PM"},
-    {value: "12:45", label: "12:45 PM"},
-    {value: "13:00", label: "01:00 PM"},
-    {value: "13:15", label: "01:15 PM"},
-    {value: "13:30", label: "01:30 PM"},
-    {value: "13:45", label: "01:45 PM"},
-    {value: "14:00", label: "02:00 PM"},
-    {value: "14:15", label: "02:15 PM"},
-    {value: "14:30", label: "02:30 PM"},
-    {value: "14:45", label: "02:45 PM"},
-    {value: "15:00", label: "03:00 PM"},
-    {value: "15:15", label: "03:15 PM"},
-    {value: "15:30", label: "03:30 PM"},
-    {value: "15:45", label: "03:45 PM"},
-    {value: "16:00", label: "04:00 PM"},
-    {value: "16:15", label: "04:15 PM"},
-    {value: "16:30", label: "04:30 PM"},
-    {value: "16:45", label: "04:45 PM"},
-    {value: "17:00", label: "05:00 PM"},
-    {value: "17:15", label: "05:15 PM"},
-    {value: "17:30", label: "05:30 PM"},
-    {value: "17:45", label: "05:45 PM"},
-    {value: "18:00", label: "06:00 PM"},
-    {value: "18:15", label: "06:15 PM"},
-    {value: "18:30", label: "06:30 PM"},
-    {value: "18:45", label: "06:45 PM"},
-    {value: "19:00", label: "07:00 PM"},
-    {value: "19:15", label: "07:15 PM"},
-    {value: "19:30", label: "07:30 PM"},
-    {value: "19:45", label: "07:45 PM"},
-    {value: "20:00", label: "08:00 PM"},
-    {value: "20:15", label: "08:15 PM"},
-    {value: "20:30", label: "08:30 PM"},
-    {value: "20:45", label: "08:45 PM"},
-    {value: "21:00", label: "09:00 PM"},
-    {value: "21:15", label: "09:15 PM"},
-    {value: "21:30", label: "09:30 PM"},
-    {value: "21:45", label: "09:45 PM"},
-    {value: "22:00", label: "10:00 PM"},
-    {value: "22:15", label: "10:15 PM"},
-    {value: "22:30", label: "10:30 PM"},
-    {value: "22:45", label: "10:45 PM"},
-    {value: "23:00", label: "11:00 PM"},
-    {value: "23:15", label: "11:15 PM"},
-    {value: "23:30", label: "11:30 PM"},
-    {value: "23:45", label: "11:45 PM"},
+    {value: "00:00:00", label: "12:00 AM"},
+    {value: "00:15:00", label: "12:15 AM"},
+    {value: "00:30:00", label: "12:30 AM"},
+    {value: "00:45:00", label: "12:45 AM"},
+    {value: "01:00:00", label: "01:00 AM"},
+    {value: "01:15:00", label: "01:15 AM"},
+    {value: "01:30:00", label: "01:30 AM"},
+    {value: "01:45:00", label: "01:45 AM"},
+    {value: "02:00:00", label: "02:00 AM"},
+    {value: "02:15:00", label: "02:15 AM"},
+    {value: "02:30:00", label: "02:30 AM"},
+    {value: "02:45:00", label: "02:45 AM"},
+    {value: "03:00:00", label: "03:00 AM"},
+    {value: "03:15:00", label: "03:15 AM"},
+    {value: "03:30:00", label: "03:30 AM"},
+    {value: "03:45:00", label: "03:45 AM"},
+    {value: "04:00:00", label: "04:00 AM"},
+    {value: "04:15:00", label: "04:15 AM"},
+    {value: "04:30:00", label: "04:30 AM"},
+    {value: "04:45:00", label: "04:45 AM"},
+    {value: "05:00:00", label: "05:00 AM"},
+    {value: "05:15:00", label: "05:15 AM"},
+    {value: "05:30:00", label: "05:30 AM"},
+    {value: "05:45:00", label: "05:45 AM"},
+    {value: "06:00:00", label: "06:00 AM"},
+    {value: "06:15:00", label: "06:15 AM"},
+    {value: "06:30:00", label: "06:30 AM"},
+    {value: "06:45:00", label: "06:45 AM"},
+    {value: "07:00:00", label: "07:00 AM"},
+    {value: "07:15:00", label: "07:15 AM"},
+    {value: "07:30:00", label: "07:30 AM"},
+    {value: "07:45:00", label: "07:45 AM"},
+    {value: "08:00:00", label: "08:00 AM"},
+    {value: "08:15:00", label: "08:15 AM"},
+    {value: "08:30:00", label: "08:30 AM"},
+    {value: "08:45:00", label: "08:45 AM"},
+    {value: "09:00:00", label: "09:00 AM"},
+    {value: "09:15:00", label: "09:15 AM"},
+    {value: "09:30:00", label: "09:30 AM"},
+    {value: "09:45:00", label: "09:45 AM"},
+    {value: "10:00:00", label: "10:00 AM"},
+    {value: "10:15:00", label: "10:15 AM"},
+    {value: "10:30:00", label: "10:30 AM"},
+    {value: "10:45:00", label: "10:45 AM"},
+    {value: "11:00:00", label: "11:00 AM"},
+    {value: "11:15:00", label: "11:15 AM"},
+    {value: "11:30:00", label: "11:30 AM"},
+    {value: "11:45:00", label: "11:45 AM"},
+    {value: "12:00:00", label: "12:00 PM"},
+    {value: "12:15:00", label: "12:15 PM"},
+    {value: "12:30:00", label: "12:30 PM"},
+    {value: "12:45:00", label: "12:45 PM"},
+    {value: "13:00:00", label: "01:00 PM"},
+    {value: "13:15:00", label: "01:15 PM"},
+    {value: "13:30:00", label: "01:30 PM"},
+    {value: "13:45:00", label: "01:45 PM"},
+    {value: "14:00:00", label: "02:00 PM"},
+    {value: "14:15:00", label: "02:15 PM"},
+    {value: "14:30:00", label: "02:30 PM"},
+    {value: "14:45:00", label: "02:45 PM"},
+    {value: "15:00:00", label: "03:00 PM"},
+    {value: "15:15:00", label: "03:15 PM"},
+    {value: "15:30:00", label: "03:30 PM"},
+    {value: "15:45:00", label: "03:45 PM"},
+    {value: "16:00:00", label: "04:00 PM"},
+    {value: "16:15:00", label: "04:15 PM"},
+    {value: "16:30:00", label: "04:30 PM"},
+    {value: "16:45:00", label: "04:45 PM"},
+    {value: "17:00:00", label: "05:00 PM"},
+    {value: "17:15:00", label: "05:15 PM"},
+    {value: "17:30:00", label: "05:30 PM"},
+    {value: "17:45:00", label: "05:45 PM"},
+    {value: "18:00:00", label: "06:00 PM"},
+    {value: "18:15:00", label: "06:15 PM"},
+    {value: "18:30:00", label: "06:30 PM"},
+    {value: "18:45:00", label: "06:45 PM"},
+    {value: "19:00:00", label: "07:00 PM"},
+    {value: "19:15:00", label: "07:15 PM"},
+    {value: "19:30:00", label: "07:30 PM"},
+    {value: "19:45:00", label: "07:45 PM"},
+    {value: "20:00:00", label: "08:00 PM"},
+    {value: "20:15:00", label: "08:15 PM"},
+    {value: "20:30:00", label: "08:30 PM"},
+    {value: "20:45:00", label: "08:45 PM"},
+    {value: "21:00:00", label: "09:00 PM"},
+    {value: "21:15:00", label: "09:15 PM"},
+    {value: "21:30:00", label: "09:30 PM"},
+    {value: "21:45:00", label: "09:45 PM"},
+    {value: "22:00:00", label: "10:00 PM"},
+    {value: "22:15:00", label: "10:15 PM"},
+    {value: "22:30:00", label: "10:30 PM"},
+    {value: "22:45:00", label: "10:45 PM"},
+    {value: "23:00:00", label: "11:00 PM"},
+    {value: "23:15:00", label: "11:15 PM"},
+    {value: "23:30:00", label: "11:30 PM"},
+    {value: "23:45:00", label: "11:45 PM"},
   ]  
     
   const gmtStrings = [
@@ -166,24 +171,26 @@ const Modal = () => {
     "#454545",
     "#999999",
   ];
-  const [color, setColor] = useState("#00B87C");
-  const [isColorBoxOpen, setIsColorBoxOpen] = useState(false);
-  const [eventTag, setEventTag] = useState("");
+  
 
-  const handleNewEventSubmit = (evt) => {
-    evt.preventDefault();
-    setOpenSnackbar(true);
-    // Check input fields
-    console.log({
+  const handleNewEventSubmit = (data) => {
+    const eventFormData = {
+      ...data,
       Event_Tag: eventTag,
       Event_Color: color,
-    });
+    }
+    
+    
+    console.log(eventFormData);
+
+    setOpenSnackbar(true);
+    // Check input fields
     // clear inputs
     setEventTag("");
     setColor("00B87C");
   };
 
-  const [openSnackbar, setOpenSnackbar] = useState(false);
+  
 
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
@@ -221,22 +228,23 @@ const Modal = () => {
             </button>
             {showEventPage ? (       
               <div className="event-tab">
-                <form onSubmit={handleNewEventSubmit} className='evenForm'>
-                <div className="firstRow">
-                    <div className="evenForm-title">
-                      <FaRegEdit className="event-field-icon"/>
+                <form onSubmit={handleSubmit(handleNewEventSubmit)} className='evenForm'>
+                  <div className="firstRow">
+                    <div className={`evenForm-title ${errors.title? 'input-error': ""}`}>
+                      <img className="event-field-icon" src={icon3} alt=""></img>
                       <input
-                        name = "title"
                         placeholder='Weekend Get Away'
+                        {...register("title", {required: "Enter Event Title"})}
                       />
                     </div>
                   </div>
                   <div className="secondRow">
-                    <div className="dateInput icon-enabled-date-picker">
-                        <FaRegCalendar className="event-field-icon"/>
+                    <div className={`dateInput icon-enabled-date-picker ${errors.startDate? 'input-error': ""}`}>
+                        <img className="event-field-icon" src={icon2} alt=""></img>
                         <Controller
                           control={control}
                           name="startDate"
+                          rules={{required: "Select a Start Date"}}
                           render={({field: {onChange, onBlur, value, ref}}) =>(
                             <DatePicker 
                               placeholderText="Start Date"
@@ -250,11 +258,12 @@ const Modal = () => {
                         />
                         <FaAngleDown className="event-field-icon"/>
                     </div>
-                    <div className="dateInput icon-enabled-date-picker">
-                        <FaRegCalendar className="event-field-icon"/>
+                    <div className={`dateInput icon-enabled-date-picker ${errors.endDate? 'input-error': ""}`}>
+                        <img className="event-field-icon" src={icon2} alt=""></img>
                         <Controller
                           control={control}
                           name="endDate"
+                          rules={{required: "Select an End Date"}}
                           render={({field: {onChange, onBlur, value, ref}}) =>(
                             <DatePicker 
                                 placeholderText="End Date"
@@ -269,9 +278,12 @@ const Modal = () => {
                     </div>
                   </div>
                   <div className="thirdRow">
-                    <div className="timeInput">
-                        <FaRegClock className="event-field-icon"/>
-                        <select name="startTime" className="time-select">
+                    <div className={`timeInput ${errors.startTime? 'input-error': ""}`}>
+                        <img className="event-field-icon" src={icon} alt=""></img>
+                        <select  
+                          className="time-select"
+                          {...register("startTime", {required: "Select a Start Time"})}
+                        >
                           <option value="">Start Time</option>
                           {
                             timeStrings.map((timeString)=>(
@@ -280,9 +292,12 @@ const Modal = () => {
                             }
                         </select>
                     </div>
-                    <div className="timeInput">
-                        <FaRegClock className="event-field-icon"/>
-                        <select name="endTime" className="time-select">
+                    <div className={`timeInput ${errors.endTime? 'input-error': ""}`}>
+                        <img className="event-field-icon" src={icon} alt=""></img>
+                        <select 
+                          className="time-select"
+                          {...register("endTime", {required: "Select an End Time"})}
+                        >
                           <option value="">End Time</option>
                           {
                             timeStrings.map((timeString)=>(
@@ -291,8 +306,11 @@ const Modal = () => {
                             }
                         </select>
                     </div>
-                    <div className="gmtInput">
-                      <select name="time" className="time-select">
+                    <div className={`gmtInput ${errors.gmt? 'input-error': ""}`}>
+                      <select
+                        className="time-select"
+                        {...register("gmt", {required: "Select GMT"})}
+                      >
                           {
                             gmtStrings.map((gmtString)=>(
                               <option value={gmtString.value}>{gmtString.label}</option>
@@ -303,7 +321,10 @@ const Modal = () => {
                   </div>
                   <div className="fourthRow">
                     <div>
-                      <input type="checkbox" name="allDay" value="Bike"/>
+                      <input
+                        type="checkbox"
+                        {...register("allDay")}
+                      />
                       <label htmlFor="allDay"> All Day</label><br/>
                     </div>
                   </div>
@@ -361,7 +382,10 @@ const Modal = () => {
                         color: "#00B87C",
                         marginRight: "1rem",
                       }}
-                      onClick={() => setIsModalOpen(false)}
+                      onClick={() =>{
+                        clearErrors()
+                        setIsModalOpen(false)
+                      }}
                       className="eventButtons__cancel"
                     >
                       Cancel
@@ -369,7 +393,6 @@ const Modal = () => {
                     <Button
                       type="submit"
                       variant="contained"
-                      onClick={handleNewEventSubmit}
                       style={{ backgroundColor: "#00B87C", color: "#fff" }}
                       className="eventButtons__create"
                     >
