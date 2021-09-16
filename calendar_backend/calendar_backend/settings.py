e5dad87e3f5b4d23e1fe985e07336a66e425b141
olowu abayomi <aayobam@gmail.com>
2021-09-16 08:19:05 +0100

    resolved cors headers errors

--- calendar_backend/calendar_backend/settings.py
+++ calendar_backend/calendar_backend/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv(str('SECRT_KEY'))
+SECRET_KEY = os.getenv(('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
