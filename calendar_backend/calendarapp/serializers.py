ddb4badf397eba12a1c8e250ff0f2e32cf6ed50e
jamido1 <66392050+jamido1@users.noreply.github.com>
2021-09-16 21:36:09 +0100

    reminder serializer

--- calendar_backend/calendarapp/serializers.py
+++ calendar_backend/calendarapp/serializers.py
@@ -4,30 +4,6 @@ from Core.utils import get_timezones, DEFAULT_TIMEZONE
 from rest_framework import serializers
 
 """
-Creating  a serializer class for events
-"""
-
-
-class EventSerializer(serializers.Serializer):
-    _id = serializers.ReadOnlyField()
-    event_title = serializers.CharField(required=True)
-    start_date = serializers.DateField(required=True)
-    end_date = serializers.DateField(required=True)
-    start_time = serializers.TimeField(required=True)
-    end_time = serializers.TimeField(required=True)
-    time_zone = serializers.ChoiceField(
-        choices=get_timezones(), default=DEFAULT_TIMEZONE)
-    description = serializers.CharField(max_length=250, required=True)
-    all_day = serializers.BooleanField(required=True)
-    event_tag = serializers.CharField(required=True)
-    event_colour = serializers.CharField(required=True)
-    images = serializers.ImageField(required=False)
-
-    def __str__(self):
-        return f"{self.event_title} created successfully"
-
-
-"""
 Creating  a serializer class for reminders.
 """
 
@@ -48,4 +24,31 @@ class ReminderSerializer(serializers.Serializer):
     date = serializers.DateField(required=True)
     time = serializers.TimeField(required=True)
     repeat = serializers.ChoiceField(required=True, choices=repeat_choices)
-    all_day = serializers.BooleanField(required=True)
+    all_day = serializers.BooleanField(required=False)
+
+
+"""
+Creating  a serializer class for events
+"""
+
+
+class EventSerializer(serializers.Serializer):
+    _id = serializers.ReadOnlyField()
+    event_title = serializers.CharField(required=True)
+    start_date = serializers.DateField(required=True)
+    end_date = serializers.DateField(required=True)
+    start_time = serializers.TimeField(required=True)
+    end_time = serializers.TimeField(required=True)
+    time_zone = serializers.ChoiceField(
+        choices=get_timezones(), default=DEFAULT_TIMEZONE)
+    description = serializers.CharField(max_length=250, required=True)
+    all_day = serializers.BooleanField(required=False)
+    event_tag = serializers.CharField(required=True)
+    event_colour = serializers.CharField(required=True)
+    reminder = serializers.ListField(allow_empty=True, child=ReminderSerializer(many=True))
+    images = serializers.ImageField(required=False)
+
+    def __str__(self):
+        return f"{self.event_title} created successfully"
+
+
