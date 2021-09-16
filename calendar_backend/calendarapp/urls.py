61d425a7ec633baf34a5d925ffebe159f8e81ce5
akanjimm <akanjimm@gmail.com>
2021-09-16 23:42:47 +0100

    detail view and url route for event

--- calendar_backend/calendarapp/urls.py
+++ calendar_backend/calendarapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('create-event/', CreateEventView.as_view(), name='create_event'),
     path('create-reminder/', CreateReminderView.as_view(), name='create-reminder'),
     path('event-list/', event_list, name="event_lists"),
+    path('event-detail/<str:id>/', event_detail_view),
     path('sidebar/', side_bar_view, name="side_bar"),
     path('info/', plugin_info_view, name="plugin_info"),
     path('ping/', ping_view, name="ping"),
