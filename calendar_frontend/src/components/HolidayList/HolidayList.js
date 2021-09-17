137fc280319e6fb083e125a1d96293f3d7a76e12
Nwankwo Henry <nameisifunanya@gmail.com>
2021-09-17 12:50:34 +0100

    Modified the show month and year state toggle

--- calendar_frontend/src/components/HolidayList/HolidayList.js
+++ calendar_frontend/src/components/HolidayList/HolidayList.js
@@ -1,13 +1,13 @@
-import React, { useContext, useEffect, useState } from "react";
-import { AppContext } from "../../App";
-import "./HolidayList.css";
-import { FiEdit2 } from "react-icons/fi";
-import { RiDeleteBin5Line } from "react-icons/ri";
+import React, { useContext, useEffect, useState } from 'react';
+import { AppContext } from '../../App';
+import './HolidayList.css';
+import { FiEdit2 } from 'react-icons/fi';
+import { RiDeleteBin5Line } from 'react-icons/ri';
 
 const HolidayList = () => {
-  const url = "https://calendar.zuri.chat/api/v1/event-list/";
+  const url = 'https://calendar.zuri.chat/api/v1/event-list/';
   const states = useContext(AppContext);
-  const { month, year } = states;
+  const { month, year, setShowMonth, setShowYear } = states;
   const [holidays, setHolidays] = useState([]);
 
   const getHolidays = async () => {
@@ -17,13 +17,13 @@ const HolidayList = () => {
   };
 
   const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
   ];
   useEffect(() => {
     getHolidays().then((data) => {
@@ -32,29 +32,35 @@ const HolidayList = () => {
   }, [url, month, year]);
 
   return (
-    <div className="home-page">
+    <div
+      className='home-page'
+      onClick={() => {
+        setShowMonth(false);
+        setShowYear(false);
+      }}
+    >
       {holidays.map((holiday) => {
         const { _id, start_date, all_day, event_title, event_colour } = holiday;
         return (
           <li
             key={_id}
-            className="holiday"
+            className='holiday'
             style={{ borderLeft: `8px solid ${event_colour}` }}
           >
-            <div className="date-icons">
-              <span className="event-date">
-                {days[new Date(start_date).getDay()]}{" "}
+            <div className='date-icons'>
+              <span className='event-date'>
+                {days[new Date(start_date).getDay()]}{' '}
                 {new Date(start_date).getDate()}
               </span>
-              <span className="edit-del">
-                <FiEdit2 style={{ marginRight: "5px" }} />
+              <span className='edit-del'>
+                <FiEdit2 style={{ marginRight: '5px' }} />
                 <RiDeleteBin5Line />
               </span>
             </div>
-            <p className="event-time" style={{ color: `${event_colour}` }}>
-              {all_day ? "All Day" : ""}
+            <p className='event-time' style={{ color: `${event_colour}` }}>
+              {all_day ? 'All Day' : ''}
             </p>
-            <p className="event-title" style={{ color: `${event_colour}` }}>
+            <p className='event-title' style={{ color: `${event_colour}` }}>
               {event_title}
             </p>
           </li>
