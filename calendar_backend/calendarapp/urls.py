08ea33b927dded3b9e183536c1297ae7b4117d53
Dosu333 <vladamir1865@gmail.com>
2021-09-18 10:04:41 +0100

    Created update event url  as per issue #351

--- calendar_backend/calendarapp/urls.py
+++ calendar_backend/calendarapp/urls.py
@@ -5,10 +5,10 @@ from .views import *
 
 urlpatterns = [
     path('', calendar_view, name="calendar_view"),
-    path('create_event/', CreateEventView().as_view()),
+    # path('create_event/', CreateEventView().as_view()),
     path('list-events/', EventListView.as_view()),
-    path('event-detail/<int:pk>/', EventDetailView.as_view(), name="evetdetail"),
-    path('update-event/<int:pk>/', update_event_view),
+    # path('event-detail/<int:pk>/', EventDetailView.as_view(), name="evetdetail"),
+    path('update-event/<str:pk>/', update_event_view),
     path('delete-event/<int:pk>/', DeleteEventView.as_view()),
     path('search/', EventSearch.as_view(), name="EventSearch"),
     path('reminders/', ReminderListView.as_view(), name='reminders'),
