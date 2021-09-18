fadfa43cb7207b95afd3af47a580f77e70479ede
Dosu333 <vladamir1865@gmail.com>
2021-09-18 17:06:33 +0100

    Fixed bug

--- calendar_backend/calendarapp/views.py
+++ calendar_backend/calendarapp/views.py
@@ -101,7 +101,7 @@ def update_event_view(request, pk):
     try:
         if serializer.is_valid(raise_exception=True):
             serialized_data = serializer.data
-            response = request.patch(url, data=serialized_data)
+            response = requests.patch(url, json=serialized_data)
 
             if response.status_code != 200:
                 return Response({'success':False, 'errors':response.json()['message']}, status=status.HTTP_400_BAD_REQUEST)
