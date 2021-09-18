5d3407a63d3ea62a512678b74b25fd5eb7453604
Wealth Iduwe <wealthiduwe@gmail.com>
2021-09-18 20:17:17 +0100

    added event popup list of event and reminders

--- calendar_frontend/src/components/HolidayList/HolidayList.js
+++ calendar_frontend/src/components/HolidayList/HolidayList.js
@@ -1,52 +1,34 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { AppContext } from '../../App';
-import './HolidayList.css';
-import { FiEdit2 } from 'react-icons/fi';
-import { RiDeleteBin5Line } from 'react-icons/ri';
+import React, { useContext } from "react";
+import { AppContext } from "../../App";
+import "./HolidayList.css";
+import { FiEdit2 } from "react-icons/fi";
+import { RiDeleteBin5Line } from "react-icons/ri";
+import EventCard from "../EventPopup/EventCard";
+import { set } from "date-fns";
 
 const HolidayList = () => {
-  const url = 'https://calendar.zuri.chat/api/v1/event-list/';
   const states = useContext(AppContext);
-  const { month, year, months, setShowMonth, setShowYear } = states;
-  const [holidays, setHolidays] = useState([]);
+  const {
+    setShowMonth,
+    setShowYear,
+    holidays,
+    days,
+    handleOverlay,
+    isEventOpen,
+    handleEventPopups,
+  } = states;
 
-  const getHolidays = async () => {
-    const response = await fetch(url);
-    const holidays = await response.json();
-    return holidays.data.slice(11);
-  };
+  // const []
 
-  const days = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-  ];
-  useEffect(() => {
-    getHolidays().then((data) => {
-      setHolidays(
-        data.filter((holiday) => {
-          return (
-            holiday.start_date.slice(0, 4) === year.toString() &&
-            months.indexOf(month) + 1 ===
-              parseInt(holiday.start_date.slice(5, 7))
-          );
-        })
-      );
-    });
-  }, [url, month, year, months]);
   return (
     <div
-      className='home-page'
+      className="home-page"
       onClick={() => {
         setShowMonth(false);
         setShowYear(false);
       }}
     >
-      {holidays.map((holiday) => {
+      {holidays.map((holiday, index) => {
         const {
           _id,
           start_date,
@@ -59,42 +41,48 @@ const HolidayList = () => {
         return (
           <li
             key={_id}
-            className='holiday'
+            className="holiday"
             style={{ borderLeft: `8px solid ${event_colour}` }}
+            onClick={() => handleEventPopups(index)}
           >
-            <div className='date-icons'>
-              <span className='event-date'>
-                {days[new Date(start_date).getDay()]}{' '}
+            <div className="date-icons">
+              <span className="event-date">
+                {days[new Date(start_date).getDay()]}{" "}
                 {new Date(start_date).getDate()}
               </span>
-              <span className='edit-del'>
-                <FiEdit2 style={{ marginRight: '5px' }} />
+              <span className="edit-del">
+                <FiEdit2 style={{ marginRight: "5px" }} />
                 <RiDeleteBin5Line />
               </span>
             </div>
-            <p className='event-time' style={{ color: `${event_colour}` }}>
+            <p className="event-time" style={{ color: `${event_colour}` }}>
               {all_day
-                ? 'All Day'
+                ? "All Day"
                 : `${
                     +start_time.slice(0, 2) >= 12
                       ? +start_time.slice(0, 2) - 12
                       : +start_time.slice(0, 2)
-                  }:${start_time.slice(3, 5).padStart(2, '0')} ${
-                    +start_time.slice(0, 2) >= 12 ? 'PM' : 'AM'
+                  }:${start_time.slice(3, 5).padStart(2, "0")} ${
+                    +start_time.slice(0, 2) >= 12 ? "PM" : "AM"
                   }-${
                     +end_time.slice(0, 2) >= 12
                       ? +end_time.slice(0, 2) - 12
                       : +end_time.slice(0, 2)
-                  }:${end_time.slice(3, 5).padStart(2, '0')} ${
-                    +end_time.slice(0, 2) >= 12 ? 'PM' : 'AM'
+                  }:${end_time.slice(3, 5).padStart(2, "0")} ${
+                    +end_time.slice(0, 2) >= 12 ? "PM" : "AM"
                   }`}
             </p>
-            <p className='event-title' style={{ color: `${event_colour}` }}>
+            <p className="event-title" style={{ color: `${event_colour}` }}>
               {event_title}
             </p>
+            <div className="pop_up">
+              {holiday.event ? <EventCard id={_id} /> : null}
+            </div>
           </li>
         );
       })}
+
+      {isEventOpen && <div onClick={handleOverlay} className="_overlay"></div>}
     </div>
   );
 };
