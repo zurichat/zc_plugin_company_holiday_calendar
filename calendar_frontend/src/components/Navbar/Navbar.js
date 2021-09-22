f86284f1ba18b5fa89562268cd7b218d3b36ca6b
Adeoye Victor <79271707+adeoyevictor@users.noreply.github.com>
2021-09-22 16:28:33 +0100

    Revert "Replaced missing font awesome icons due to migration with react-icons"

--- calendar_frontend/src/components/Navbar/Navbar.js
+++ calendar_frontend/src/components/Navbar/Navbar.js
@@ -1,13 +1,13 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../../Plugin';
-import './Navbar.css';
-import { BiCalendar } from 'react-icons/bi';
-import { FaBars } from 'react-icons/fa';
-import { RiArrowDropDownLine } from 'react-icons/ri';
-import { AiOutlinePlus } from 'react-icons/ai';
+import React, { useContext } from 'react'
+import { AppContext } from '../../Plugin'
+import './Navbar.css'
+import { BiCalendar } from 'react-icons/bi'
+import { FaBars } from 'react-icons/fa'
+import { RiArrowDropDownLine } from 'react-icons/ri'
+import { AiOutlinePlus } from 'react-icons/ai'
 
 const Navbar = () => {
-  const states = useContext(AppContext);
+  const states = useContext(AppContext)
   const {
     month,
     setMonth,
@@ -18,7 +18,7 @@ const Navbar = () => {
     showYear,
     setShowYear,
     handleModal,
-  } = states;
+  } = states
   const months = [
     {
       id: 'January',
@@ -68,28 +68,28 @@ const Navbar = () => {
       id: 'December',
       name: 'Dec',
     },
-  ];
-  const currentYear = new Date().getFullYear();
-  const startYear = currentYear;
+  ]
+  const currentYear = new Date().getFullYear()
+  const startYear = currentYear
   return (
     <>
       <nav
         className='nav'
         onClick={() => {
           if (showMonth !== false) {
-            setShowMonth(false);
+            setShowMonth(false)
           }
           if (showMonth !== false) {
-            setShowYear(false);
+            setShowYear(false)
           }
         }}
       >
         <div
           className='calendar'
           onClick={() => {
-            setShowMonth(!showMonth);
+            setShowMonth(!showMonth)
             if (showYear !== false) {
-              setShowYear(false);
+              setShowYear(false)
             }
           }}
         >
@@ -108,13 +108,13 @@ const Navbar = () => {
         <div className='grpHolder'>
           <div className='yearHeadGrp' onClick={() => setShowYear(!showYear)}>
             <h3 className='yearHeadItem'>{year}</h3>
-            <RiArrowDropDownLine className='fal fa-angle-down' />
+            <i className='fal fa-angle-down'></i>
           </div>
           <div
             className='monthGrp'
             onClick={() => {
               if (showYear !== false) {
-                setShowYear(false);
+                setShowYear(false)
               }
             }}
           >
@@ -124,12 +124,12 @@ const Navbar = () => {
                   className='monthItem'
                   key={month.id}
                   onClick={() => {
-                    setMonth(month.id);
+                    setMonth(month.id)
                   }}
                 >
                   {month.name}
                 </span>
-              );
+              )
             })}
           </div>
         </div>
@@ -143,20 +143,20 @@ const Navbar = () => {
                   className='yearItem'
                   key={startYear + i}
                   onClick={() => {
-                    setYear(startYear + i);
-                    setShowMonth(!showMonth);
-                    setShowYear(!showYear);
+                    setYear(startYear + i)
+                    setShowMonth(!showMonth)
+                    setShowYear(!showYear)
                   }}
                 >
                   {startYear + i}
                 </li>
-              );
+              )
             })}
           </ul>
         </div>
       )}
     </>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
